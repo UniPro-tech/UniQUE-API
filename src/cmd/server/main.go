da6203bf67f1a4470aa606c200774a339804b16a
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-02-04 21:41:41 +0900

    初期設定

---
+++ src/cmd/server/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/UniPro-tech/UniQUE-API/internal/config"
+	"github.com/UniPro-tech/UniQUE-API/internal/db"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/swag/example/basic/docs"
+	"gorm.io/gorm/logger"
+
+	"github.com/gin-gonic/gin"
+)
+
+type HealthResponse struct {
+	Status string `json:"status"`
+}
+
+// @BasePath /
+
+// HealthCheck godoc
+// @Summary health check endpoint
+// @Schemes
+// @Description システムの稼働状況を確認するためのエンドポイントです。
+// @Tags system info
+// @Accept json
+// @Produce json
+// @Success 200 {object} HealthResponse
+// @Router /health [get]
+func healthCheck(c *gin.Context) {
+	c.JSON(http.StatusOK, HealthResponse{
+		Status: "ok",
+	})
+}
+
+func main() {
+	environmentConfigs := config.LoadConfig()
+
+	// Initialize database
+	dbConnection, err := db.NewDB()
+	dbConnection.Logger = dbConnection.Logger.LogMode(logger.Info)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// loggerとrecoveryミドルウェア付きGinルーター作成
+	r := gin.Default()
+
+	// Swagger Info
+	docs.SwaggerInfo.BasePath = "/"
+	docs.SwaggerInfo.Title = environmentConfigs.AppName + " Auth API"
+	docs.SwaggerInfo.Version = environmentConfigs.Version
+
+	// Add contexts
+	r.Use(func(c *gin.Context) {
+		c.Set("config", environmentConfigs)
+		c.Set("db", dbConnection)
+		c.Next()
+	})
+
+	// Routes
+	r.GET("/health", healthCheck)
+
+	// Start server
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	r.Run()
+}
---
+++ src/internal/config/env.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"os"
+)
+
+type Config struct {
+	AppName     string
+	Version     string
+	FrontendURL string
+	IssuerURL   string
+}
+
+// envが設定されていない場合のデフォルト値
+var (
+	Version   = "latest"
+	GitCommit = "unknown"
+	GitBranch = "unknown"
+)
+
+var (
+	AppName     = "UniQUE"
+	FrontendURL = "http://localhost:3000"
+	IssuerURL   = "http://localhost:8080"
+)
+
+func LoadConfig() *Config {
+	version := Version
+
+	if Version == "latest" {
+		version = GitBranch + "@" + GitCommit
+	} else {
+		version = Version + "+" + GitCommit
+	}
+
+	// envから設定を読み込む
+	AppNameEnv := os.Getenv("CONFIG_APP_NAME")
+	if AppNameEnv == "" {
+		AppNameEnv = AppName
+	}
+	FrontendURLEnv := os.Getenv("CONFIG_FRONTEND_URL")
+	if FrontendURLEnv == "" {
+		FrontendURLEnv = FrontendURL
+	}
+	IssuerURLEnv := os.Getenv("CONFIG_ISSUER_URL")
+	if IssuerURLEnv == "" {
+		IssuerURLEnv = IssuerURL
+	}
+	return &Config{
+		AppName:     AppNameEnv,
+		FrontendURL: FrontendURLEnv,
+		IssuerURL:   IssuerURLEnv,
+		Version:     version,
+	}
+}
